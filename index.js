83361014201c510424040dcd1b4a8bd6b868bca7
chiaraferrara <chiara.ferrara@stevejobs.academy>
2024-02-28 17:56:53 +0100

    feat : railway port setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 const server = http.createServer(app);
+const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
   try {
@@ -15,6 +16,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log("Server running!");
 });
